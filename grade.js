f41b0a4795c272827caa8db8d416f7a3d2a95a74
Bennet Owino <bennetkowin09@gmail.com>
2024-12-18 10:15:01 +0300

    code

---
+++ grade.js
@@ -0,0 +1,27 @@
+
+let grade = 40;
+
+
+if (grade >= 0 && grade < 40) {
+    console.log("Your Grade Is E");
+}
+
+else if (grade >= 40 && grade < 50) {
+    console.log("Your Grade Is D");
+}
+
+else if (grade >= 50 && grade < 60) {
+    console.log("Your Grade Is C");
+}
+
+else if (grade >= 60 && grade < 80) {
+    console.log("Your Grade Is B");
+}
+
+else if (grade >= 80 && grade <= 100) {
+    console.log("Your Grade Is A");
+}
+
+else {
+    console.log("Invalid Input");
+}
